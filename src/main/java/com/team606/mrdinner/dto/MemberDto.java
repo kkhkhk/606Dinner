ca734f087612f36e9230204c0d632c9e5c5c479f
Hwang Soojin <soojin9433@naver.com>
2025-09-11 00:57:37 +0900

    MemberDto.java에서 주소 필드 추가

--- src/main/java/com/team606/mrdinner/dto/MemberDto.java
+++ src/main/java/com/team606/mrdinner/dto/MemberDto.java
@@ -3,9 +3,11 @@ package com.team606.mrdinner.dto;
 public class MemberDto {
     private String username;
     private String password;
+    private String address;
 
-    public MemberDto(String username, String password) {
+    public MemberDto(String username, String password, String address) {
         this.username = username;
         this.password = password;
+        this.address = address;
     }
 }
