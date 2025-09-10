ac3a5bbffa8184f2c0ca508d1167ba26dc584f7d
Hwang Soojin <soojin9433@naver.com>
2025-09-11 00:21:34 +0900

    first commit

---
+++ src/main/java/com/team606/mrdinner/MrdinnerApplication.java
@@ -0,0 +1,13 @@
+package com.team606.mrdinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrdinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrdinnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team606/mrdinner/MrdinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team606.mrdinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrdinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
