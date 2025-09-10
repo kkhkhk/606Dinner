8ed59c58d1940f060b69319a53128ac3771964ac
Hwang Soojin <soojin9433@naver.com>
2025-09-11 01:03:03 +0900

    MemberDto.java에서 address 필드 삭제

--- src/main/java/com/team606/mrdinner/dto/MemberDto.java
+++ src/main/java/com/team606/mrdinner/dto/MemberDto.java
@@ -3,11 +3,9 @@ package com.team606.mrdinner.dto;
 public class MemberDto {
     private String username;
     private String password;
-    private String address;
 
-    public MemberDto(String username, String password, String address) {
+    public MemberDto(String username, String password) {
         this.username = username;
         this.password = password;
-        this.address = address;
     }
 }
